45189637530ccf2de7c85d35086523dc9e32f100
homaander <hom.ander23@gmail.com>
2024-08-29 01:08:37 +0300

    Ok

--- src/HomaCode.hs
+++ src/HomaCode.hs
@@ -24,9 +24,9 @@ module HomaCode (
   , getLoopId
   , getLoop
 
-  , haveIn500l
-  , haveIn500r
-  , showIn500r
+  , findDisperce
+  , findDisperceData
+  , showDisperceList
 
   {-
   , findLoops
@@ -45,33 +45,28 @@ import Data.List ( nub, sort )
 
 type HData = [Int]
 
-t0,t1,t2,t3,t4,t5,t6,t7,t8,t9 :: HData
-t0 = [0,0,0,0,0]
-t1 = [0,0,0,0,1]
-t2 = [0,0,0,0,2]
-t3 = [0,0,0,0,3]
-t4 = [0,0,0,0,4]
-t5 = [0,0,0,0,5]
-t6 = [0,0,0,0,6]
-t7 = [0,0,0,0,7]
-t8 = [0,0,0,0,8]
-t9 = [0,0,0,0,9]
+-- Разложение лент
+disperceMaxOffset :: Int
+disperceMaxOffset = 500
 
--- Суммы лент
-showIn500r :: HData -> HData -> [Int]
-showIn500r aTape bTape = sort $ nub $ map (\n -> fromHData $ getLoopId (sumData aTape (codeN n bTape))) [0 .. 500]
+showDisperceList :: HData -> HData -> [Int]
+showDisperceList aTape bTape = sort $ nub $ map (\n -> fromHData $ getLoopId (sumData aTape (codeN n bTape))) [0 .. disperceMaxOffset]
 
-haveIn500r :: HData -> HData -> HData -> [Int]
-haveIn500r aTape bTape resTape = filter (>= 0) $
-    map (\n -> if getLoopId (sumData aTape (codeN n bTape)) == lid then n else -1) [0 .. 500]
+findDisperce :: HData -> HData -> HData -> [Int]
+findDisperce aTape bTape resTape = filter (>= 0) $
+    map (\n -> if getLoopId (sumData aTape (codeN n bTape)) == lid then n else -1) [0 .. disperceMaxOffset]
   where
     lid = getLoopId resTape
 
-haveIn500l :: HData -> HData -> HData -> [Int]
-haveIn500l aTape bTape resTape = filter (>= 0) $
-    map (\n -> if getLoopId (sumData (codeN n aTape) bTape) == lid then n else -1) [0 .. 500]
+findDisperceData :: HData -> HData -> HData -> [(Int,Int)]
+findDisperceData aTape bTape resTape = filter (/= (0,0)) $
+    map check [0 .. disperceMaxOffset]
   where
     lid = getLoopId resTape
+    thr (_, _, a, _) = a
+    check n = if getLoopId nsum == lid then (n, thr $ getLoop nsum) else (0,0)
+      where
+        nsum = sumData aTape (codeN n bTape)
 
 -- Ленты
 getLoopLen :: HData -> Int
@@ -170,6 +165,19 @@ sub a b = add a (neg b)
 
 neg :: Int -> Int
 neg n   = (10 - n) `mod` 10
+
+-- Тестовые значения
+t0,t1,t2,t3,t4,t5,t6,t7,t8,t9 :: HData
+t0 = [0,0,0,0,0]
+t1 = [0,0,0,0,1]
+t2 = [0,0,0,0,2]
+t3 = [0,0,0,0,3]
+t4 = [0,0,0,0,4]
+t5 = [0,0,0,0,5]
+t6 = [0,0,0,0,6]
+t7 = [0,0,0,0,7]
+t8 = [0,0,0,0,8]
+t9 = [0,0,0,0,9]
 
 {-
 findLoops :: Int -> [(HData, Int)]
